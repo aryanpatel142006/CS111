2b44bd243f1a94f0fa983004d5363e5a68455518
Aryan Patel <aryanpatel142006@gmail.com>
2024-11-13 20:31:28 -0500

    QuadraticKoch half done

--- Homework/Assignment6/QuadraticKoch.java
+++ Homework/Assignment6/QuadraticKoch.java
@@ -22,6 +22,32 @@ public class QuadraticKoch {
      */
     public static double[][] getCoords(double x0, double y0, double x5, double y5) {
         // WRITE YOUR CODE HERE
+        double dBetweenPoints = (x5-x0)/3; // distance between the each x cordinate
+        // if (x0 == x5){
+        // double[][] coordinates = {{x0 /* index0 */,x0+dBetweenPoints /* index1 */,x0+dBetweenPoints /* index2 */,x0+dBetweenPoints+dBetweenPoints /* index3 */,x0+dBetweenPoints+dBetweenPoints /* index4 */,x5/* index5 */},
+        // {y0, y0, y0 + dBetweenPoints, y0 + dBetweenPoints, y5, y5}};
+        // }else {
+        // creating a 2D array [2][5] 
+        double[][] coordinates = {{x0 /* index0 */,x0+dBetweenPoints /* index1 */,x0+dBetweenPoints /* index2 */,x0+dBetweenPoints+dBetweenPoints /* index3 */,x0+dBetweenPoints+dBetweenPoints /* index4 */,x5/* index5 */},
+                                {y0, y0, y0 + dBetweenPoints, y0 + dBetweenPoints, y5, y5}};
+        //}   
+        
+        // double[][] coordinates = new double[2][6];
+        // double dx = x5 - x0;
+        // double dy = y5 - y0;
+        // coordinates[0][0] = x0;
+        // coordinates[1][0] = y0;
+        // coordinates[0][1] = x0 + dx / 4;
+        // coordinates[1][1] = y0 + dy / 4;
+        // coordinates[0][2] = x0 + dx / 2;
+        // coordinates[1][2] = y0 + dy / 2;
+        // coordinates[0][3] = x0 + dx * 3 / 4;
+        // coordinates[1][3] = y0 + dy * 3 / 4;
+        // coordinates[0][4] = x0 + dx;
+        // coordinates[1][4] = y0 + dy;
+
+
+        return coordinates;
     }
 
     /**
@@ -37,6 +63,26 @@ public class QuadraticKoch {
      */
     public static void koch(double x0, double y0, double x5, double y5, int n) {
         // WRITE YOUR CODE HERE
+        // getting coordinates to create a segment of koch from getCoords method
+        double[][] coords = getCoords(x0, y0, x5, y5); 
+        //test
+        System.out.println("------- NEW ------");
+        for ( int i = 0; i < 5; i++){
+            System.out.print(coords[0][i] + " <- x y -> ");
+            System.out.println(coords[1][i]);
+
+        }
+        //test
+        // base case if n == 0
+        if ( n == 0){
+            StdDraw.line(x0, y0,x5, y5);
+            return;
+        }
+        koch(coords[0][0], coords[1][0], coords[0][1], coords[1][1], n-1);
+        koch(coords[0][1], coords[1][1], coords[0][2], coords[1][2], n-1);
+        koch(coords[0][2], coords[1][2], coords[0][3], coords[1][3], n-1);
+        koch(coords[0][3], coords[1][3], coords[0][4], coords[1][4], n-1);
+        koch(coords[0][4], coords[1][4], coords[0][5], coords[1][5], n-1);
     }
 
     /**
@@ -48,5 +94,15 @@ public class QuadraticKoch {
      */
     public static void main(String[] args) {
         // WRITE YOUR CODE HERE
+
+        int order = Integer.parseInt(args[0]); // taking valuse of order from command line input
+
+        // calling kock 4 times with different coordinates
+        koch(0.25, 0.75, 0.75, 0.75, order);// success
+        koch(0.25, 0.25, 0.25, 0.75, order);
+        koch(0.75, 0.75, 0.75, 0.25, order);
+        koch(0.75, 0.25 ,0.25, 0.25, order);// success
+
+         //koch(0.25, 0.25, 0.25, 0.75, order);
     }
 }
